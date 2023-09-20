6d155e4ff9f6ddcbf06a07b707558e2093bd78bd
Rachit <merachit2898@gmail.com>
2023-09-20 10:47:04 +0530

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const connectWithDb = require("./config/db");
+const serverless = require("serverless-http");
+const router = express.Router();
+
 require("dotenv").config();
 const app = express();
 const { PORT } = process.env;
@@ -11,12 +14,16 @@ connectWithDb();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// use express router
-app.use("/", require("./routes"));
+app.use("./.netlify/functions/api", router);
+module.exports = app;
+module.exports.handler = serverless(app);
 
-app.listen(PORT || 2000, (err) => {
-  if (err) {
-    console.log(`Error in running the server : ${err}`);
-  }
-  console.log(`Server is up and running at ${PORT}`);
-});
+// use express router
+// app.use("/", require("./routes"));
+
+// app.listen(PORT || 2000, (err) => {
+//   if (err) {
+//     console.log(`Error in running the server : ${err}`);
+//   }
+//   console.log(`Server is up and running at ${PORT}`);
+// });
